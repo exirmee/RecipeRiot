fbb1fcba61dfb90e77d2be949dfb0507dea1ad21
exirmee <exir.mee@gmail.com>
2023-03-19 01:49:29 +0330

    stativ

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -125,11 +125,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
